40a73fa919af283274812d01999cad9a26633b8d
Luke Arnould <35514663+lukearnould@users.noreply.github.com>
2024-07-27 14:57:51 -0500

    Drier context invocation

--- HospitalManagement/Repository.cs
+++ HospitalManagement/Repository.cs
@@ -5,27 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using HospitalManagement.Config;
 
 namespace HospitalManagement
 {
     public class Repository(ICoreConfig config)
     {
+        private HospitalContext DefaultContext()
+        {
+            return new HospitalContext(config.SQLConnectionString);
+        }
+
 
         public async Task<List<Hospital>> Get()
         {
-            using var db = new HospitalContext(config.SQLConnectionString);
+            using var db = DefaultContext();
             return await db.Hospital.ToListAsync();
         }
 
         public async Task<Hospital?> Get(int id)
         {
-            using var db = new HospitalContext(config.SQLConnectionString);
+            using var db = DefaultContext();
             return await db.Hospital.FindAsync(id);
         }
 
         public async Task Save(Hospital hospital)
         {
-            using var db = new HospitalContext(config.SQLConnectionString);
+            using var db = DefaultContext();
 
             if (hospital.HospitalId == 0)
             {
@@ -40,8 +46,8 @@ namespace HospitalManagement
 
         public async Task Delete(int id)
         {
-            using var db = new HospitalContext(config.SQLConnectionString);
-            
+            using var db = DefaultContext();
+
             Hospital hospital = new() { HospitalId = id };
             db.Hospital.Attach(hospital);
             db.Remove(hospital);
