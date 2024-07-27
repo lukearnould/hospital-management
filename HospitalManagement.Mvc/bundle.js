13793c5893b78220f3b7864e299d0465ae71480a
Luke Arnould <35514663+lukearnould@users.noreply.github.com>
2024-07-27 16:49:21 -0500

    esbuildify

---
+++ HospitalManagement.Mvc/bundle.js
@@ -0,0 +1,25 @@
+﻿const esbuild = require('esbuild');
+
+function bundleCSS() {
+    esbuild.build({
+        logLevel: 'info',
+        entryPoints: ['wwwroot/css/app.css'],
+        bundle: true,
+        minify: true,
+        outfile: 'wwwroot/css/app.bundle.css'
+    }).catch(() => process.exit(1));
+}
+
+function bundleJS() {
+    esbuild.build({
+        logLevel: 'info',
+        entryPoints: ['wwwroot/js/app.js'],
+        bundle: true,
+        minify: true,
+        sourcemap: true,
+        outfile: 'wwwroot/js/app.bundle.js'
+    }).catch(() => process.exit(1));
+}
+
+bundleCSS();
+bundleJS();
---
+++ HospitalManagement.Mvc/wwwroot/js/app.js
@@ -0,0 +1,5 @@
+﻿import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+window.bootstrap = bootstrap;
+
+import TomSelect from '../../node_modules/tom-select/dist/js/tom-select.complete.js';
+window.TomSelect = TomSelect;
