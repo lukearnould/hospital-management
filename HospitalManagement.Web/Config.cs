c874caad6a94697979fed76f9b2731fcd975d17c
Luke Arnould <35514663+lukearnould@users.noreply.github.com>
2024-07-30 17:37:54 -0500

    Code cleanup

--- HospitalManagement.Web/Config.cs
+++ HospitalManagement.Web/Config.cs
@@ -5,7 +5,7 @@ namespace HospitalManagement.Web
     public class Config(IConfiguration config) : ICoreConfig
     {
         public string ConnectionString { get; set; } = config.GetValue<string>("config:connectionstring")
-            + " Password=" + config["Database__Password"] + ";" 
+            + " Password=" + config["Database__Password"] + ";"
             ?? throw new Exception("Unable to locate connection string and/or database user secret in appsettings.json/secrets.json");
     }
 }
