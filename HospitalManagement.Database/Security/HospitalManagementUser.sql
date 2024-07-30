14d434607d53f0655e97ace0b081f244771604ad
Luke Arnould <35514663+lukearnould@users.noreply.github.com>
2024-07-30 18:16:15 -0500

    Merge SQL creation files for user

--- HospitalManagement.Database/Security/HospitalManagementUser.sql
+++ HospitalManagement.Database/Security/HospitalManagementUser.sql
@@ -1,2 +1,24 @@
-﻿CREATE USER [HospitalManagementUser] FOR LOGIN [HospitalManagementUser];
+﻿CREATE LOGIN [HospitalManagementUser] WITH PASSWORD = ''
+go
 
+CREATE USER [HospitalManagementUser] FOR LOGIN [HospitalManagementUser];
+go
+
+GRANT VIEW ANY COLUMN ENCRYPTION KEY DEFINITION TO PUBLIC;
+GO
+
+GRANT VIEW ANY COLUMN MASTER KEY DEFINITION TO PUBLIC;
+GO
+
+GRANT CONNECT TO [HospitalManagementUser];
+go
+
+ALTER ROLE [db_accessadmin] ADD MEMBER [HospitalManagementUser];
+GO
+
+ALTER ROLE [db_datareader] ADD MEMBER [HospitalManagementUser];
+go
+
+
+ALTER ROLE [db_datawriter] ADD MEMBER [HospitalManagementUser];
+go
--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 dotnet user-secrets set "Database__Password" "<your password goes here>"
 ```
 
- 4. Update the password set in `HospitalManagement.Database.Security.HospitalManagementUser_1.sql` to match the password you entered above.
+ 4. Update the password set in `HospitalManagement.Database.Security.HospitalManagementUser.sql` to match the password you entered above.
  5. Publish the `HospitalManagement.Database` project to your database.
  6. Run the below command at the root of the `HospitalManagement.Web` project:
 
