04340047714df581424bbb7038b293a4d06b6069
Luke Arnould <35514663+lukearnould@users.noreply.github.com>
2025-02-17 15:01:25 -0600

    Constructor with validations

--- HospitalManagement/Models/Hospital.cs
+++ HospitalManagement/Models/Hospital.cs
@@ -1,9 +1,52 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
 
 namespace HospitalManagement.Models
 {
     public record Hospital
     {
+        public Hospital() { }
+
+        public Hospital(string name, string description, string color, string phoneNumber, string emailAddress, string url)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("Name cannot be null or empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new Exception("Description cannot be null or empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(color))
+            {
+                throw new Exception("Color cannot be null or empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(phoneNumber) || phoneNumber.Length > 10)
+            {
+                throw new Exception("Phone number must be 10 characters or less and cannot be empty");
+            }
+
+            if (!MailAddress.TryCreate(emailAddress, out MailAddress? parsedEmail))
+            {
+                throw new Exception("Email is an invalid format");
+            }
+
+            if (!Uri.TryCreate(url, UriKind.Absolute, out Uri? parsedUri))
+            {
+                throw new Exception("Invalid URI");
+            }
+
+            Name = name;
+            Description = description;
+            Color = color;
+            PhoneNumber = phoneNumber;
+            EmailAddress = parsedEmail;
+            URL = parsedUri;
+        }
+
         [Key]
         public int HospitalId { get; set; }
 
@@ -28,9 +71,9 @@ namespace HospitalManagement.Models
 
         [EmailAddress]
         [StringLength(100)]
-        public string EmailAddress { get; set; } = "";
+        public MailAddress EmailAddress { get; set; }
 
         [StringLength(100)]
-        public string URL { get; set; } = "";
+        public Uri URL { get; set; }
     }
 }
